6fcea5b4515afc368cd9c8a4d054976b7d1ae550
ljz <ljz@jiuqi.com.cn>
2021-01-16 18:06:30 +0800

    dir search

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod version;
 
-use std::{error::Error, fmt::Display, path::Path};
+use std::{error::Error, io::ErrorKind, path::{Path, PathBuf}};
+use std::fmt::Display;
 
 use clap::{App, Arg};
 
@@ -10,11 +11,12 @@ pub struct Config {
     dir: String,
     backup: String,
     verbose: bool,
+    test: bool,
 }
 
 impl Display for Config {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[dir = {}, backup = {}, verbose = {}]", self.dir, self.backup, self.verbose)
+        write!(f, "[dir = {}, backup = {}, verbose = {}, test = {}]", self.dir, self.backup, self.verbose, self.test)
     }
 }
 
@@ -36,11 +38,16 @@ impl Config {
                             .short("v")
                             .long("verbose")
                             .help("Use verbose output"))
+                        .arg(Arg::with_name("test")
+                            .short("t")
+                            .long("test")
+                            .help("Scan and find the duplicated plugins, but do nothing"))   
                         .get_matches();
 
         let dir = matches.value_of("DIR").unwrap();
         let backup = matches.value_of("BACKUP").unwrap();
         let verbose = matches.is_present("verbose");
+        let test = matches.is_present("test");
 
         let root_path = Path::new(&dir);
         if !root_path.is_dir() {
@@ -57,13 +64,54 @@ impl Config {
         Ok(Config {
             dir: String::from(dir),
             backup: String::from(backup),
-            verbose
+            verbose,
+            test
         })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("{}", config);
+    scan_plugins(&config.dir, config.verbose)?;
 
     Ok(())
+}
+
+macro_rules! log {
+    ($enabled:expr) => {
+        {if $enabled { println!(); }}
+    };
+    ($enabled:expr, $($arg:tt)*) => {
+        {if $enabled { println!($($arg)*); }}
+    };
+}
+
+fn scan_plugins(dir: &str, verbose: bool) -> Result<(), Box<dyn Error>> {
+    let plugin_path: PathBuf = [dir, "plugins"].iter().collect();
+    if !plugin_path.is_dir() { 
+        let e = std::io::Error::new(ErrorKind::NotFound, format!("Can not find `plugins` dir under `{}` dir", dir));
+        return Err(Box::new(e));
+    }
+
+    log!(verbose, "Search plugins under dir `{}`...", plugin_path.display());
+    for entry in plugin_path.read_dir()? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            log!(verbose, ">> {:?}", path);
+        }
+    }
+    
+    Ok(())
+}
+
+struct Plugin {
+    path: PathBuf,
+    name: String,
+    version: Version,
+}
+
+impl Plugin {
+    // fn new(path: PathBuf) -> Result<Plugin, Box<dyn Error>> {
+
+    // }
 }
